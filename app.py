11477c66ae06b862b6255e99b2c811b1530ac70e
Hari kishan <nivedithahl05@gmail.com>
2026-01-12 20:55:22 +0530

    UI updates / buttons / fixes

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'yourusername@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'password1234')
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'your-email@gmail.com')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'your-app-password')
 
